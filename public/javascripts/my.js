ccb340ef216358c0bbb9afb2c8d4f09a70f3a258
yama <yama.tsuyo@gmail.com>
2012-06-16 17:43:27 +0900

    backboneサンプルデータ挿入まで

--- public/javascripts/my.js
+++ public/javascripts/my.js
@@ -1,33 +1,72 @@
-console.log('tehteh');
+$(function(){
+  var Todo = Backbone.Model.extend({
+    defaults: function(){
+      return {
+        title: "empty todo..."
+      };
+    },
 
-var Todo = Backbone.Model.extend({
-  initialize: function(){
+    initialize: function(){
+    },
+  });
 
-  },
-  defaults: {
-    name: 'name',
-  }
-});
+  var TodoList = Backbone.Collection.extend({
+    model: Todo,
+    localStorage: new Store("todos-backbone"),
+  });
 
-var todo = new Todo({});
-console.log(todo.get('name'));
+  var Todos = new TodoList;
 
-var YamaView = Backbone.View.extend({
-  el: $(".yama"),
+  var TodoView = Backbone.View.extend({
+    tagName: "li",
 
-  initialize: function(){
-    console.log('initialize');
-    this.render();
-  },
-  render: function(){
-    console.log('render');
+    initialize: function(){
+      this.render();
+    },
 
-//    $(".yama").html('hogehoge');
-    this.$el.html('hogehoge');
-    return this;
-  }
-});
+    render: function(){
+      this.$el.html('<li>' + this.model.get("title")  + '</li>');
+      return this;
+    }
+      
+  });
 
-var view = new YamaView;
-console.log(view.el);
-console.log($(view.el).html)
+  var AppView = Backbone.View.extend({
+    el: $("#todoapp"),
+
+    events: {
+      "keypress #new-todo": "addTodo"
+    },
+
+    initialize: function(){
+      this.input = this.$("#new-todo");
+
+      Todos.bind("add", this.addOne, this);
+      Todos.bind("all", this.render, this);
+
+      this.render();
+    },
+
+    render: function(){
+      if (Todos.length){
+
+      } else{
+
+      }
+      $("#yama").html(Todos.length);
+    },
+
+    addOne: function(todo){
+      var view = new TodoView({model: todo});
+      this.$("#todo-list").append(view.render().el);
+    },
+
+    addTodo: function(e){
+      if(e.keyCode != 13) return;
+      Todos.create({title: this.input.val()});
+      this.input.val('');
+    },
+  });
+
+  var App = new AppView;  
+})
